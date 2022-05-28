d1b445ca739fbb27a21a76a79366359ef068ad99
merve haciabbasoglu <dev.mervehaciabbasoglu@gmail.com>
2022-05-28 18:13:15 +0300

    saturday

---
+++ lib/pages/google_sign_in.dart
@@ -0,0 +1,16 @@
+/*import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/goodle_sign_in.dart';
+
+class GoogleSignInProvider extends ChangeNotifier {
+  final googleSignIn = GoogleSignIn();
+
+  GoogleSignInAccount? _user;
+
+  GoogleSignInAccount get user => _user!;
+
+  Future googleLogin() async{
+    final googleUser = await googleSignIn.signIn();
+
+  }
+}*/
